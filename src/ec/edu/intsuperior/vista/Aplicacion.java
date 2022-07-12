2f224a3f0ba0159b9c6211f2ac2480504c9799eb
Anderson Patricio <quishpe.anderson@intsuperior.edu.ec>
2022-07-12 00:30:08 -0500

    Version de Anderson Quishpe

--- src/ec/edu/intsuperior/vista/Aplicacion.java
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -2,11 +2,20 @@
 package ec.edu.intsuperior.vista;
 
 import javax.swing.JOptionPane;
-
+import ec.edu.intsuperior.modelo.Personal;
+import java.util.Scanner;
 
 public class Aplicacion {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Este es el segmento de aquishpe");
+        Scanner leer= new Scanner(System.in);
+        Personal p1= new Personal();
+        System.out.println("Ingresa la cedula");
+        p1.setCi(leer.next());
+        System.out.println("Ingresa Nombre1");
+        p1.setNombre1(leer.next());
+        p1.setNombre2(JOptionPane.showInputDialog("Ingresa tu segundo nombre"));
+        p1.setApellido1(JOptionPane.showInputDialog("Ingresa el apellido paterno"));
+        JOptionPane.showMessageDialog(null,p1.toString());
     }
     
 }
